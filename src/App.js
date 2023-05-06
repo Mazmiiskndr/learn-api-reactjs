42009d475d0c4b018e3732fa9be7eca653ff5b36
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-05-06 18:55:26 +0700

    :bookmark: Release

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import ImageList from "components/ImageList";
 
 export default function App() {
   const [images, setImages] = useState([]);
+  const [notFound, setNotFound] = useState(false);
 
   const handleSubmit = async (searchTerm, e) => {
     e.preventDefault();
     const results = await searchImages(searchTerm);
     setImages(results);
+
+    if (results.length === 0) {
+      setNotFound(true);
+    } else {
+      setNotFound(false);
+    }
   };
 
   return (
     <div className="bg-gray-100 min-h-screen">
       <SearchBar onSubmit={handleSubmit} />
+      {notFound && (
+        <p className="text-center text-red-500">Gambar tidak ditemukan.</p>
+      )}
       <ImageList images={images} />
     </div>
   );
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -2,6 +2,13 @@
 import React from "react";
 import ImageShow from "./ImageShow";
 
+/**
+ * This is a functional component that renders a grid of images using the props passed to it.
+ * @returns A React component that renders a grid of images using the `ImageShow` component for each
+ * image in the `images` array passed as a prop. The grid has 4 columns and a gap of 4. The `ImageShow`
+ * component is passed the `image` object as a prop and a unique `key` prop based on the `id` property
+ * of the `image` object.
+ */
 const ImageList = ({ images }) => {
   return (
     <div className="grid grid-cols-4 gap-4 p-20">
